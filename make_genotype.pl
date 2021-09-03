9a1e3f815ecc787513090049fb9259a5a70a7e3f
Jenny Mathew <35796432+JennyMMathew@users.noreply.github.com>
2021-09-03 10:27:27 +0200

    Create make_genotype.pl
    To create the genotype file

---
+++ make_genotype.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+my ($genotype) = @ARGV;
+
+open (IN1,"<", "$genotype") or die "Cannot open $genotype \n";
+open (OUT,">tmp") or die "Cannot write into tmp \n";
+#open (HEADER,">head.txt") or die "Cannot write into head.txt \n";
+@geno=<IN1>;
+chomp @geno;
+
+	
+for ($i=0;$i<@geno;$i++)
+{
+	$geno[$i]=~s/\r//g;
+	$geno[$i]=~s/AA/0/g;
+	$geno[$i]=~s/AB/1/g;
+	$geno[$i]=~s/BB/2/g;
+	$geno[$i]=~s/"//g;
+	$geno[$i]=~s/-\d*_._._\w+:.//g;
+	print OUT "$geno[$i]\n";
+}
